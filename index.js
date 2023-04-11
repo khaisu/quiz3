6390dc0da15193ea07e39a8600aa479e7def53f3
Muhammad Khairul <onlykay0.0@gmail.com>
2023-04-11 15:13:30 +0800

    login

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json())
-app.post('/login', (req,res) =>{
-    console.log(req.body)
-    
-    res.send('login')
-})
-
 app.get('/', (req, res) => {
   res.send('DRAGON IS COMING!')
 })
@@ -20,3 +13,75 @@ app.get('/bruh', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.use(express.json())
+
+app.post('/login', (req,res) =>{
+    console.log(req.body)
+    
+let result = login(req.body.username,req.body.password)
+
+    //res.send('login')
+    res.send(result)
+})
+
+//app.use(express.json())
+app.post('/register', (req,res) =>{
+    console.log(req.body)
+    
+    let result = register(req.body.username,req.body.password,req.body.name,req.body.email)
+    //res.send('login')
+    res.send(result)
+})
+
+let dbUsers=[
+
+  {
+      username:"khairul",
+      password:"1234",
+      name:"Khairul",
+      email:"khairul0425@gmail.com"
+  },
+
+  {
+      username:"zaid",
+      password:"1111",
+      name:"Zaid",
+      email:"zaid@gmail.com"
+  },
+
+  {
+      username:"lan",
+      password:"1234",
+      name:"Fadzlan",
+      email:"lan@gmail.com"
+  }
+]
+
+function login(reqUsername, reqPassword)
+{
+let matchUser = dbUsers.find(user => user.username == reqUsername)
+
+if(!matchUser) return "User not found"
+if(matchUser.password == reqPassword)
+{
+
+  return matchUser
+}
+else
+{
+    return" invalid"
+}
+
+}
+
+function register(reqUsername,reqPassword,reqName,reqEmail)
+{
+
+    dbUsers.push({username:reqUsername,
+        password : reqPassword,
+        name : reqName,
+        email:reqEmail
+      }
+    )
+}
